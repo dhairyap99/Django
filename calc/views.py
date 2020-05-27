1e98c37a0388ffbd1533b3d7fc2d5f5ec30336d4
dhairyap99 <dhairya.parekh@somaiya.edu>
2020-05-27 19:27:37 +0530

    GET/POST

--- calc/views.py
+++ calc/views.py
@@ -5,3 +5,11 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name':'Dhairya'});
+
+def add(request):
+
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
+    val3 = val1 + val2
+
+    return render(request, 'result.html', {'result': val3})
