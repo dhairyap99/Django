4f6aabcf5e380a6566d10a11a64f3a72261f6954
dhairyap99 <dhairya.parekh@somaiya.edu>
2020-05-28 11:28:29 +0530

    Loading destinations dynamically

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,46 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    dest1 = Destination()    
+    dest1.name = 'Bali'
+    dest1.desc = 'The land of something for everyone'
+    dest1.img = 'destination_1.jpg'
+    dest1.price = 700
+
+    dest2 = Destination()
+    dest2.name = 'Indonesia'
+    dest2.desc = 'Wonderful Indonesia'
+    dest2.img = 'destination_2.jpg'
+    dest2.price = 300
+
+    dest3 = Destination()
+    dest3.name = 'San Francisco'
+    dest3.desc = 'All within your reach'
+    dest3.img = 'destination_3.jpg'
+    dest3.price = 900
+
+    dest4 = Destination()
+    dest4.name = 'Paris'
+    dest4.desc = 'Rendez vous en France'
+    dest4.img = 'destination_4.jpg'
+    dest4.price = 800
+
+    dest5 = Destination()
+    dest5.name = 'Phi Phi Island'
+    dest5.desc = 'The most enchanting island of Thailand'
+    dest5.img = 'destination_5.jpg'
+    dest5.price = 400
+
+    dest6 = Destination()
+    dest6.name = 'Mykonos'
+    dest6.desc = 'Dance, drink, never sleep'
+    dest6.img = 'destination_6.jpg'
+    dest6.price = 600
+
+    dests = [dest1, dest2, dest3, dest4, dest5, dest6]
+  
+    return render(request, 'index.html', {'dests': dests})
