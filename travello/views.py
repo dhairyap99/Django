f564b2d35e3ea7ab79e500485e789b39556ce59b
dhairyap99 <dhairya.parekh@somaiya.edu>
2020-05-28 12:49:22 +0530

    Destinations made dynamic

--- travello/views.py
+++ travello/views.py
@@ -10,36 +10,42 @@ def index(request):
     dest1.desc = 'The land of something for everyone'
     dest1.img = 'destination_1.jpg'
     dest1.price = 700
+    dest1.offer = False
 
     dest2 = Destination()
     dest2.name = 'Indonesia'
     dest2.desc = 'Wonderful Indonesia'
     dest2.img = 'destination_2.jpg'
     dest2.price = 300
+    dest2.offer = True
 
     dest3 = Destination()
     dest3.name = 'San Francisco'
     dest3.desc = 'All within your reach'
     dest3.img = 'destination_3.jpg'
     dest3.price = 900
+    dest3.offer = False
 
     dest4 = Destination()
     dest4.name = 'Paris'
     dest4.desc = 'Rendez vous en France'
     dest4.img = 'destination_4.jpg'
     dest4.price = 800
+    dest4.offer = False
 
     dest5 = Destination()
     dest5.name = 'Phi Phi Island'
     dest5.desc = 'The most enchanting island of Thailand'
     dest5.img = 'destination_5.jpg'
     dest5.price = 400
+    dest5.offer = True
 
     dest6 = Destination()
     dest6.name = 'Mykonos'
     dest6.desc = 'Dance, drink, never sleep'
     dest6.img = 'destination_6.jpg'
     dest6.price = 600
+    dest6.offer = False
 
     dests = [dest1, dest2, dest3, dest4, dest5, dest6]
   
